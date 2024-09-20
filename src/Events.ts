206776d942b911246cd311d3e6fc7704833d15cd
diegofmo0802 <diegofmo0802@gmail.com>
2024-09-20 04:04:20 +0000

    added the possibility of using once events

--- src/Events.ts
+++ src/Events.ts
@@ -6,14 +6,26 @@
  */
 export class Events {
     private listeners: Events.ListenerList = new Map();
+    private onceListeners: Events.ListenerList = new Map();
     /**
      * Adds an event to the EventManager.
      * @param name The name of the event.
      * @param callback The callback that will be executed.
      */
     public on(name: string, callback: Events.CallBack): void {
-        if (!this.listeners.has(name)) this.listeners.set(name, new Set());
-        this.listeners.get(name)?.add(callback);
+        const listeners = this.listeners.get(name) ?? new Set();
+        listeners.add(callback);
+        this.listeners.set(name, listeners);
+    }
+    /**
+     * Adds an once event to the EventManager.
+     * @param name The name of the event.
+     * @param callback The callback that will be executed.
+     */
+    public once(name: string, callback: Events.CallBack): void {
+        const listeners = this.onceListeners.get(name) ?? new Set();
+        listeners.add(callback);
+        this.onceListeners.set(name, listeners);
     }
     /**
      * Removes an event from the EventManager.
@@ -21,17 +33,33 @@ export class Events {
      * @param callback The callback of the event to remove.
      */
     public off(name: string, callback: Events.CallBack): void {
-        if (!this.listeners.has(name)) return;
-        if (!this.listeners.get(name)?.has(callback)) return;
-        this.listeners.get(name)?.delete(callback);
+        const listenerList = this.listeners.get(name);
+        if (!listenerList) return;
+        listenerList.delete(callback);
     }
     /**
-     * Removes all callbacks from an event.
+     * Removes an once event from the EventManager.
+     * @param name The name of the event to remove.
+     * @param callback The callback of the event to remove.
+     */
+    public offOnce(name: string, callback: Events.CallBack): void {
+        const listenerList = this.onceListeners.get(name);
+        if (!listenerList) return;
+        listenerList.delete(callback);
+    }
+    /**
+     * Removes all listeners from an event.
      * @param name The name of the event from which the callbacks will be removed.
      */
     public offAll(name: string): void {
-        if (!this.listeners.has(name)) return;
         this.listeners.delete(name);
+    }
+    /**
+     * Removes all listeners from an once event.
+     * @param name The name of the event from which the callbacks will be removed.
+     */
+    public offAllOnce(name: string): void {
+        this.onceListeners.delete(name);
     }
     /**
      * Executes an event.
@@ -39,8 +67,10 @@ export class Events {
      * @param args The arguments that will be passed to the callbacks.
      */
     protected dispatch(name: string, ...args: any[]): void {
-        if (!this.listeners.has(name)) return;
-        this.listeners.get(name)?.forEach((callback) => callback(...args));
+        const listeners = this.listeners.get(name);
+        const onceListeners = this.onceListeners.get(name);
+        if (listeners) listeners.forEach(listener => listener(...args));
+        if (onceListeners) onceListeners.forEach(listener => listener(...args));
     }
     /**
      * Returns the number of callbacks of an event.
@@ -48,10 +78,9 @@ export class Events {
      * @returns The number of callbacks of the event.
      */
     protected eventCount(name: string): number {
-        if (!this.listeners.has(name)) return 0;
-        const events = this.listeners.get(name);
-        if (!events) return 0;
-        return events.size;
+        const listenerCount = this.listeners.get(name)?.size ?? 0;
+        const onceCount     = this.onceListeners.get(name)?.size ?? 0;
+        return listenerCount + onceCount;
     }
 }
 export namespace Events {
